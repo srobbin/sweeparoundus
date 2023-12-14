59d485dad54eb64e9fcf949ba30951e22366fa3d
Carl Hauck <carlhauck@gmail.com>
2023-12-14 09:40:28 -0600

    adds ransackable_attributes and ransackable_associations for activeadmin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,8 @@
 class AdminUser < ApplicationRecord
   devise :database_authenticatable, :lockable,
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[email current_sign_in_at sign_in_count created_at]
+  end
 end
