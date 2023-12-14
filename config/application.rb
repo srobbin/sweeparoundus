6d293d9b220ca77e7d07013dbfb6922efe7b2c49
Carl Hauck <carlhauck@gmail.com>
2023-12-14 01:57:01 -0600

    config tweaks

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,23 @@ module Sweeparoundus
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # UUIDs
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
+    # Redis cache
+    config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"], db: 0 }
+
+    # Sidekiq
+    config.active_job.queue_adapter = :sidekiq
+
+    # Time zone
+    config.time_zone = ENV.fetch("DEFAULT_TIMEZONE") { "Central Time (US & Canada)" }
+
+    # Host
+    config.active_job.default_url_options = { host: ENV["SITE_URL"] }
   end
 end
