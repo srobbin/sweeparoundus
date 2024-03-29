b8271520e10a0cc3dec6cb109b4bdf053600db4a
Carl Hauck <carlhauck@gmail.com>
2024-03-29 17:39:51 -0500

    save lat and lng if user wants to save street address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_14_012803) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_29_220223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_14_012803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "street_address"
+    t.decimal "lat", precision: 10, scale: 6
+    t.decimal "lng", precision: 10, scale: 6
     t.index ["area_id"], name: "index_alerts_on_area_id"
   end
 
