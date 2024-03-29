903665951c6ec6ea2c2b6d97f922a12c48aa8f6a
Carl Hauck <carlhauck@gmail.com>
2024-03-29 18:35:47 -0500

    schedules live

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_action :set_note_header
   before_action :set_note
 
-  NEW_SCHEDULES_LIVE = false
+  NEW_SCHEDULES_LIVE = true
 
   def index
   end
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
     @note_header =
       if sweeping_done_for_year?
         "SWEEP YOU NEXT YEAR"
-      elsif NEW_SCHEDULES_LIVE && !is_january_thru_march?
+      elsif NEW_SCHEDULES_LIVE && !is_january_or_feb?
         "#{current_year} SCHEDULES NOW LIVE"
       else
         "#{current_year} SCHEDULES COMING SOON"
@@ -22,10 +22,10 @@ class HomeController < ApplicationController
 
   def set_note
     @note = "Please note that alert subscriptions do not carry over from year to year, unless you save your street address when signing up for notifications."
-    if NEW_SCHEDULES_LIVE && !is_january_thru_march?
+    if NEW_SCHEDULES_LIVE && !is_january_or_feb?
       @note += " If your street address has changed, simply subscribe with your new address and then unsubscribe your old address from an alert email."
     elsif !sweeping_done_for_year? && !NEW_SCHEDULES_LIVE
-      @note += " New schedules will be posted after the City publishes them (typically in late March or early April), at which point all alert subscriptions that are either unconfirmed or without street addresses will be deleted."
+      @note += " New schedules will be posted after the City publishes them (typically in late March), at which point all alert subscriptions that are either unconfirmed or without street addresses will be deleted."
     end
   end
 
@@ -37,8 +37,8 @@ class HomeController < ApplicationController
     is_month_in?([12])
   end
 
-  def is_january_thru_march?
-    is_month_in?([1, 2, 3])
+  def is_january_or_feb?
+    is_month_in?([1, 2])
   end
 
   def is_month_in?(months)
