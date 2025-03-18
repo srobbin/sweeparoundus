d4c9353ccf386ec77504916b226f45f27e467c37
Carl Hauck <carlhauck@gmail.com>
2025-03-17 20:41:15 -0500

    mailer refactoring and tweaks

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,4 +1,15 @@
 class AlertMailer < ApplicationMailer
+  before_action :set_alert_and_area, only: [:reminder, :confirm, :deleted_notification]
+  before_action :set_email, only: [:reminder, :confirm, :deleted_notification]
+  before_action :set_street_address, only: [:reminder, :confirm]
+  before_action :set_formatted_address_area, only: [:confirm]
+  before_action :set_disclaimer, only: [:reminder, :confirm]
+  before_action :set_sweep_dates, only: [:reminder]
+  before_action :set_mailer_urls, only: [:confirm, :reminder]
+  before_action :set_home_url, only: [:deleted_notification]
+
+  DISCLAIMER = "Note: This site does not guarantee that the information presented is accurate, or that notifications will be delivered on a timely basis. Please consult the Department of Streets and Sanitation website, your Ward's / alderperson's website, and street signage for up-to-date parking information."
+
   def yearly
     mail(
       to: params[:email],
@@ -7,59 +18,66 @@ class AlertMailer < ApplicationMailer
   end
 
   def reminder
-    @sweep = params[:sweep]
-    @alert = params[:alert]
-    @area = @alert.area
-    @street_address = @alert.street_address
-    @dates = [
-      @sweep.date_1,
-      @sweep.date_2,
-      @sweep.date_3,
-      @sweep.date_4,
-    ].compact.map { |d| d.strftime("%B %-d") }
-    @unsubscribe_url = unsubscribe_area_alerts_url(@area, t: encode_jwt(@alert.email, @street_address))
-
     mail(
-      to: @alert.email,
+      to: @email,
       subject: "Street sweeping alert for #{@area.name}",
     )
   end
 
   def confirm
-    @alert = params[:alert]
-    @area = @alert.area
-    @email = @alert.email
-    @street_address = @alert.street_address
-    @formatted_address_area = formatted_address_area(@street_address, @area)
-
-    token = encode_jwt(@email, @street_address)
-    @confirmation_url = confirm_area_alerts_url(@area, t: token)
-    @unsubscribe_url = unsubscribe_area_alerts_url(@area, t: token)
-
     mail(
-      to: @alert.email,
+      to: @email,
       subject: "Please confirm your subscription to #{@area.name}",
     )
   end
 
   def deleted_notification
-    @alert = params[:alert]
-    @area = @alert.area
-    @home_url = root_url
-
     mail(
-      to: @alert.email,
+      to: @email,
       subject: "Your street sweeping alert subscription has been canceled",
     )
   end
 
   private
 
-  def formatted_address_area(street_address, area)
-    if street_address
-      "#{street_address} (#{area.name})"
-    else
-      area.name
-    end
+  def set_alert_and_area
+    @alert = params[:alert]
+    @area = @alert.area
+  end
+
+  def set_email
+    @email = @alert.email
+  end
+
+  def set_street_address
+    @street_address = @alert.street_address
+  end
+
+  def set_formatted_address_area
+    @formatted_address_area = @street_address ? "#{@street_address} (#{@area.name})" : @area.name
+  end
+
+  def set_disclaimer
+    @disclaimer = DISCLAIMER
+  end
+
+  def set_sweep_dates
+    @sweep = params[:sweep]
+    @dates = [
+      @sweep.date_1,
+      @sweep.date_2,
+      @sweep.date_3,
+      @sweep.date_4,
+    ].compact.map { |d| d.strftime("%B %-d") }
+  end
+
+  def set_mailer_urls
+    token = encode_jwt(@email, @street_address)
+    @confirmation_url = confirm_area_alerts_url(@area, t: token)
+    @unsubscribe_url = unsubscribe_area_alerts_url(@area, t: token)
+  end
+
+  def set_home_url
+    @home_url = root_url
   end
 end
