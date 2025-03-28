cd03daef8c6406df4572aa9ce8f54368fe9d1234
Carl Hauck <carlhauck@gmail.com>
2025-03-28 13:52:09 -0500

    updates zones and schedule

--- spec/data/zones_validation_spec.rb
+++ spec/data/zones_validation_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 require 'rgeo'
 require 'rgeo/geo_json'
 
-CBD_WARD_SECTION = "34CBD"
+# "On the business district item, the answer I got is that it was renamed from 34CBD to 34BD. There is no sweeping data associated, but it is there for shape purposes."
+BUSINESS_DISTRICT_WARD_SECTION = "34BD"
 
 RSpec.describe 'Zone file validation' do
   let(:file_path) { "db/data/Street Sweeping Zones - #{year}.geojson" }
@@ -13,14 +14,14 @@ RSpec.describe 'Zone file validation' do
 
     File.open(file_path, "r") do |f|
       RGeo::GeoJSON.decode(f, geo_factory: RGeo::Cartesian.simple_factory).each do |object|
-        is_cbd_ward_section = object.properties["ward_section"] == CBD_WARD_SECTION
+        is_bd_ward_section = object.properties["ward_section"] == BUSINESS_DISTRICT_WARD_SECTION
         
         ward_section = object.properties["ward_section"]
-        has_ward_section = ward_section.to_i > 0 || is_cbd_ward_section
+        has_ward_section = ward_section.to_i > 0 || is_bd_ward_section
         ward = object.properties["ward"]
         has_ward = ward.to_i > 0
         section = object.properties["section"]
-        has_section = section.to_i > 0 || is_cbd_ward_section
+        has_section = section.to_i > 0 || is_bd_ward_section
         area_shape = object.geometry
 
         unless [has_ward, has_section, area_shape].all?
@@ -28,7 +29,7 @@ RSpec.describe 'Zone file validation' do
         end
 
         # Validate WARD SECTION length
-        errors << "Invalid WARD SECTION length: #{ward_section}" unless ward_section.length == 4 || is_cbd_ward_section
+        errors << "Invalid WARD SECTION length: #{ward_section}" unless ward_section.length == 4 || is_bd_ward_section
 
         # Validate WARD SECTION against WARD
         errors << "Invalid WARD SECTION v. WARD: #{ward_section} v. #{ward}" unless ward_section[0..1] == ward
