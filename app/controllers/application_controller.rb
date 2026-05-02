784382ea75ba299c9990bc3916ad9e424ddc3364
Carl Hauck <carlhauck@gmail.com>
2026-05-02 15:15:39 -0500

    Referrer-Policy header revert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :set_security_headers
-
-  private
-
-  def set_security_headers
-    response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
-  end
 end
