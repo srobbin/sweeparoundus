e5137efc9eed80c7c3df76bf6a8fa8c3b4ae70a2
Carl Hauck <chauck@tines.io>
2026-05-12 10:50:07 -0500

    adds Sentry logging

--- app/jobs/cleanup_stale_cdot_permits_job.rb
+++ app/jobs/cleanup_stale_cdot_permits_job.rb
@@ -1,7 +1,10 @@
 class CleanupStaleCdotPermitsJob < ApplicationJob
   queue_as :default
 
-  retry_on StandardError, wait: :polynomially_longer, attempts: 3
+  retry_on StandardError, wait: :polynomially_longer, attempts: 3 do |_job, error|
+    Rails.logger.error("[CleanupStaleCdotPermitsJob] All retries exhausted: #{error.class}: #{error.message}")
+    Sentry.capture_exception(error)
+  end
 
   def perform
     result = CleanupStaleCdotPermits.new.call
