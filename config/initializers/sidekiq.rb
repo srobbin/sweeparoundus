b5dddb052fc91caf84438918e9c90fba3d0c202c
Carl Hauck <carlhauck@gmail.com>
2026-05-12 22:30:08 -0500

    sidekiq initializer update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,11 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"], db: 1, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-
-  # Cron
-  schedule_file = "config/cron.yml"
-  if File.exist?(schedule_file)
-    jobs = YAML.load_file(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash(jobs) if jobs
-  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDIS_URL"], db: 1, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
+Sidekiq::Cron.configure do |config|
+  config.cron_schedule_file = "config/cron.yml"
 end
